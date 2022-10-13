d49bf48c2f77a2e9140ae44ac3a0c72116e569b4
SamChandran <91770355+SamChandran@users.noreply.github.com>
2022-10-13 11:22:37 +0530

    initial commit 1

--- Main.py
+++ Main.py
@@ -1,14 +1,16 @@
 from typing import List
-def insertionSort(array) -> List[int]:
-  for step in range(1, len(array)):
-    key = array[step]
-    j = step - 1
-    while j >= 0 and key < array[j]:
-      array[j + 1] = array[j]
-      j = j - 1
-    array[j + 1] = key
-  return array
 
+def insertionSort(array) -> List[int]:
+  # Write your code here
+  for x in range(1,len(array)):
+    selection = x
+    for y in range(x-1,-1,-1):
+      if (array[y] > array[selection]):
+        array[y],array[selection] = array[selection],array[y]
+        selection -=1
+  return array
+      
+# data = [9, 5, 1, 4, 3]
 input_data = input()
 data = []
 for item in input_data.split(', '):
